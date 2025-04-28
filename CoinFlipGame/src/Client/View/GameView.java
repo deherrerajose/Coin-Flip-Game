1b05ebb8e80c9a8d8dea380813681097a75ffb6e
Zay DeHerrera <zay.deherrera@western.edu>
2025-04-28 12:26:13 -0600

    Created Client Project Structure

--- CoinFlipGame/src/Client/View/GameView.java
+++ CoinFlipGame/src/Client/View/GameView.java
@@ -1,4 +1,4 @@
-package View;
+package Client.View;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
--- CoinFlipGame/src/Main.java
+++ CoinFlipGame/src/Main.java
@@ -1,4 +1,4 @@
-import View.*;
+import Client.View.*;
 
 public class Main {
     public static void main(String[] args) {
